3c523bcb53c33af0325f8103ab5b08147e7ef5c1
Tristan <muell573@uwm.edu>
2018-11-23 18:11:05 -0600

    Reimplemented all functions from sprint 1

--- Commands.py
+++ Commands.py
@@ -1,4 +1,5 @@
 from TASchedulingApp import TASchedulingApp
+from DjangoTAApp.models import User, Courses, Labs
 
 app = TASchedulingApp()
 
@@ -15,10 +16,53 @@ class CommandHandler():
                 return "Logged In"
             else:
                 return "Could not login"
+        elif args[0] == "Logout":
+            if app.logout():
+                return "Logged Out"
+            else:
+                return "Could Not Logout"
         elif args[0] == "CreateAccount":
             if app.createAccount(args[1], args[2], int(args[3])):
                 return "Created New Account"
             else:
                 return "Could Not Create New Account"
+        elif args[0] == "EditAccount":
+            if app.editAccount(args[1], args[2], args[3], int(args[4])):
+                return "Edited Account"
+            else:
+                return "Could Not Edit Account"
+        elif args[0] == "CreateCourse":
+            if app.createCourse(args[1], args[2], args[3]):
+                return "Created New Course"
+            else:
+                return "Could Not Create New Course"
+        elif args[0] == "CreateLab":
+            if app.createLab(int(args[1]), int(args[2]), args[3]):
+                return "Created New Lab"
+            else:
+                return "Could Not Create New Lab"
+        elif args[0] == "DeleteAccount":
+            if app.deleteAccount(args[1]):
+                return "Deleted Account"
+            else:
+                return "Could Not Delete Account"
+        elif args[0] == "DisplayAccounts":
+            users = list(User.objects.all())
+            out = ""
+            for user in users:
+                out += "(" + user.username + ", " + user.password + ", " + str(user.clearance) + ")"
+            return out
+        elif args[0] == "DisplayCourses":
+            courses = list(Courses.objects.all())
+            out = ""
+            for course in courses:
+                out += "(" + str(course.courseID) + ", " + course.coursename + ", " + course.professor + ")"
+            return out
+        elif args[0] == "DisplayLabs":
+            labs = list(Labs.objects.all())
+            out = ""
+            for lab in labs:
+                out += "(" + str(lab.LabID) + ", " + str(lab.courseID) + ", " + lab.tausername + ")"
+            return out
         else:
             return "Error"
--- TASchedulingApp.py
+++ TASchedulingApp.py
@@ -1,89 +1,76 @@
 from User import User
 from Course import Course
-from DjangoTAApp.models import User,Courses,Labs
+from DjangoTAApp.models import User, Courses, Labs
 
 class TASchedulingApp:
-  LoggedInUser = None
-  def __init__(self):
-    self.LoggedInUser = None
+    LoggedInUser = None
+    def __init__(self):
+        self.LoggedInUser = None
 
-  def login(self, sUsername, sPassword):
-    users = list(User.objects.filter(username=sUsername))
-    for user in users:
-        if user.password == sPassword:
-            self.LoggedInUser = User(sUsername, sPassword, user.clearance)
-            return True
-    return False
-
-  def createAccount(self,sUsername,sPassword,iClearance):
-
-    if self.LoggedInUser is not None and self.LoggedInUser.clearance < 3 and "," not in sUsername and "," not in sPassword and isinstance(iClearance, int):
+    def login(self, sUsername, sPassword):
         users = list(User.objects.filter(username=sUsername))
-        if len(users) > 0:
-            return False
-        else:
-            user = User(username=sUsername, password=sPassword, clearance=iClearance)
-            user.save()
-            return True
-      
-  def editAccount(self, tUsername, nUsername, nPassword, nClearance):
-    if self.LoggedInUser is not None and self.LoggedInUser.clearance < 3:
-        file_object = open("Accounts.txt", "r+")
-        lines = file_object.readlines()
-        file_object.seek(0)
-        for i in lines:
-            if tUsername not in i:
-                file_object.write(i)
+        for user in users:
+            if user.password == sPassword:
+                self.LoggedInUser = User(sUsername, sPassword, user.clearance)
+                return True
+        return False
+
+    def logout(self):
+        self.LoggedInUser = None
+        return True
+
+    def createAccount(self,sUsername,sPassword,iClearance):
+
+        if self.LoggedInUser is not None and self.LoggedInUser.clearance < 3 and "," not in sUsername and "," not in sPassword and isinstance(iClearance, int):
+            users = list(User.objects.filter(username=sUsername))
+            if len(users) > 0:
+                return False
             else:
-                file_object.write(nUsername + "," + nPassword + "," + nClearance + "\n")
-        file_object.truncate()
-        file_object.close()
-        print("Edited Account")
-    else:
-        print("Invalid command or insufficient access")
+                user = User(username=sUsername, password=sPassword, clearance=iClearance)
+                user.save()
+                return True
 
-  def createCourses(self, uniqId, courseNumber, Professor):
-    if self.LoggedInUser is not None and self.LoggedInUser.clearance < 2:
-      course = Course(uniqId, courseNumber, Professor)
-      file_object = open("Courses.txt", "a")
-      file_object.write(uniqId + "," + courseNumber + "," + Professor + "\n")
-      file_object.close()
-      print("Created Course")
-      return True
-    else:
-      print("Invalid command")
-      return False
+    def editAccount(self, sUsername, sNewUsername, sPassword, iClearance):
+        if self.LoggedInUser is not None and self.LoggedInUser.clearance < 3:
+            users = list(User.objects.filter(username=sUsername))
+            if len(users) == 1:
+                User.objects.filter(username=sUsername).delete()
+                u1 = User(username=sNewUsername, password=sPassword, clearance=iClearance)
+                u1.save()
+                return True
+            else:
+                return False
+        else:
+            return False
 
-  def createLab(self,sTA,icourse,ilab):
-    
-    if self.LoggedInUser is not None and self.LoggedInUser.clearance < 3 :
+    def createCourse(self, uniqId, coursename, professor):
+        if self.LoggedInUser is not None and self.LoggedInUser.clearance < 2:
+            courses = list(Courses.objects.filter(courseID=uniqId))
+            if len(courses) > 0:
+                return False
+            else:
+                course = Courses(courseID=uniqId, coursename=coursename, professor=professor)
+                course.save()
+                return True
+        else:
+            return False
 
-      file_object = open("Labs.txt","a")
-      file_object.write(ilab + "," + icourse + "," + sTA + "\n")
-      file_object.close()
-      print("Lab Created")
+    def createLab(self, iLabID, iCourseId, sTA):
+        if self.LoggedInUser is not None and self.LoggedInUser.clearance < 3:
+            labs = list(Labs.objects.filter(LabID=iLabID))
+            if len(labs) > 0:
+                return False
+            else:
+                lab = Labs(LabID=iLabID, courseID=iCourseId, tausername=sTA)
+                lab.save()
+                return True
+        else:
+            return False
 
-    else:
 
-      print("Invalid command")
-
-      
-  def deleteAccount(self, sUsername):
-    
-    file_object = open("Accounts.txt", "r")
-    usernames = file_object.readlines()
-    file_object.close()
-    file_object = open("Accounts.txt", "w")
-    deleted = False
-    for line in usernames:
-      user = line.split(",")
-      if(user[0]!=sUsername):
-        file_object.write(line)
-      else:
-        deleted = True
-    if(deleted == False):
-        print("No account " + sUsername + " found")
-        return "No account " + sUsername + " found"
-    else:
-        print("account " + sUsername + " deleted")
-        return "account " + sUsername + " deleted"
+    def deleteAccount(self, sUsername):
+        if len(list(User.objects.filter(username=sUsername))) > 0:
+            User.objects.filter(username=sUsername).delete()
+            return True
+        else:
+            return False
