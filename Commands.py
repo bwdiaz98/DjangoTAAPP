25b9d1c81727f3b4a0fd26de975c233187cbc090
dummer-isaiah <44410836+dummer-isaiah@users.noreply.github.com>
2018-11-27 00:08:21 -0600

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -55,7 +55,7 @@ class CommandHandler():
         elif args[0] == "DisplayLabs":
             return app.displayLabs()
         elif args[0] == "EditCourse":
-          if app.editCourse(args[1], args[2], args[3]):
+          if app.editCourse(args[1], args[2], args[3],args[4]):
               return "Edited Course"
           else:
               return "Could Not Edit Course"
